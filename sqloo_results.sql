1ca19039664971525b6972d4665183845eadc0b3
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-11-13 12:35:55 -0800

    [sql] some work done

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -1,54 +1,90 @@
 -- 0 SELECT basics
--- 1.
+-- 1. 
+SELECT population FROM world
+  WHERE name = 'Germany'
 
 -- 2.
+SELECT name, population FROM world
+  WHERE name IN ('Sweden','Norway','Denmark');
 
 -- 3.
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
 
 -- 1 SELECT name
 
 -- 1.
-
+SELECT name FROM world
+  WHERE name LIKE 'Y%'
 -- 2.
-
+SELECT name FROM world
+  WHERE name LIKE '%Y'
 -- 3.
-
+SELECT name FROM world
+  WHERE name LIKE '%x%'
 -- 4.
-
+SELECT name FROM world
+  WHERE name LIKE '%land'
 -- 5.
-
+SELECT name FROM world
+  WHERE name LIKE 'C%ia'
 -- 6.
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
 
 -- 7.
-
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
 -- 8.
-
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
 -- 9.
-
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
 -- 10.
-
+SELECT name FROM world
+ WHERE name LIKE '____'
 -- 11.
-
+SELECT name
+  FROM world
+ WHERE name = capital
 -- 12.
+SELECT name
+  FROM world
+ WHERE capital = concat(name, ' City')
 
 -- 13, 14, and 15 have a bug, please skip them
 
 
 -- 2 SELECT from World
+
 -- 1.
-
+SELECT name, continent, population FROM world
 -- 2.
-
+SELECT name FROM world
+WHERE population > 200000000
 -- 3.
-
+SELECT name, gdp/population
+FROM world
+WHERE population > 200000000
 -- 4.
-
+SELECT name, population/1000000
+FROM world
+WHERE continent = 'South America'
 -- 5.
-
+SELECT name, population
+FROM world
+WHERE name IN ('France','Germany','Italy')
 -- 6.
-
+SELECT name
+FROM world
+WHERE name LIKE 'United%'
 -- 7.
-
+SELECT name,population,area
+FROM world
+WHERE area>3000000
+OR population>250000000
 -- 8.
 
 -- 9.
