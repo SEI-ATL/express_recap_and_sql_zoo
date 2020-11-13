dcd8e04e7337b82aa6b8c782cdc0c5d2c3bf550a
alexjbustillos <alexjbustillos@gmail.com>
2020-11-13 18:51:47 -0500

    [NOBEL]answered

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -113,33 +113,65 @@ WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%
 
 -- 3 SELECT from Nobel
 -- 1.
-
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
 -- 2.
-
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
 -- 3.
-
+SELECT yr, subject
+FROM nobel
+WHERE winner = 'Albert Einstein'
 -- 4.
-
+SELECT winner
+FROM nobel
+WHERE subject = 'Peace' AND yr >=2000;
 -- 5.
-
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Literature' AND yr >= 1980 AND yr <= 1989;
 -- 6.
-
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter', 'Barack Obama');
 -- 7.
-
+SELECT winner
+FROM nobel
+WHERE winner LIKE 'John%';
 -- 8.
-
+SELECT *
+FROM nobel
+WHERE subject = 'Physics' AND yr = 1980 OR subject = 'Chemistry' AND yr = 1984;
 -- 9.
-
+SELECT *
+FROM nobel
+WHERE yr = 1980 AND subject <> 'Chemistry' AND subject <>'Medicine';
 -- 10.
-
+SELECT *
+FROM nobel
+WHERE subject = 'Medicine' AND yr <1910 OR subject = 'Literature' AND yr >=2004;
 -- 11.
-
+SELECT *
+FROM nobel
+WHERE winner = 'Peter Grünberg';
 -- 12.
-
+SELECT *
+FROM nobel
+WHERE winner = 'Eugene O''Neill';
 -- 13.
-
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'sir%'
+ORDER BY yr DESC, winner
 -- 14.
-
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject IN ('Physics','Chemistry'), subject,winner
 
 
 -- 4 SELECT within SELECT
