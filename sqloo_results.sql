8833a29854fb7b0e343ef258556b0b2e794258a1
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-11-13 12:12:29 -0800

    started tutorial

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -1,20 +1,32 @@
 -- 0 SELECT basics
--- 1.
+--1.
+SELECT population FROM world
+  WHERE name = 'Germany'
 
--- 2.
+--2. 
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
--- 3.
+--3.
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
 
 -- 1 SELECT name
 
 -- 1.
+SELECT name, continent, population FROM world
 
 -- 2.
-
+SELECT name FROM world
+WHERE population >= 200000000
 -- 3.
-
+SELECT name, gdp/population
+FROM world
+WHERE population >= 200000000
 -- 4.
-
+SELECT name, population/1000000
+FROM world
+WHERE continent = 'South America';
 -- 5.
 
 -- 6.
