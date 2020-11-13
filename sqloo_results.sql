132a2f7b0a4a8ce31f00df77fdce23b116f8d21b
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-13 14:22:45 -0500

    [sql] Initial commit

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -1,10 +1,15 @@
 -- 0 SELECT basics
 -- 1.
-
+SELECT population FROM world
+  WHERE name = 'Germany'
 -- 2.
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
 -- 3.
-
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
+  
 -- 1 SELECT name
 
 -- 1.
