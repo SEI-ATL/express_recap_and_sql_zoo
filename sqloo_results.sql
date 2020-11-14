562d7f8d52cf3847525e912d3f8f9cd675d2a56e
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-11-14 18:00:15 -0500

    [cleaning up so I have questions and answers if I ever need to refer back to it in the future]

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -1,30 +1,50 @@
 -- 0 SELECT basics
--- 1. SELECT population FROM world
---  WHERE name = 'Germany'
+-- 1. The example uses a WHERE clause to show the population of 'France'. Note that strings (pieces of text that are data) should be in 'single quotes';Modify it to show the population of Germany
+    SELECT population FROM world
+    WHERE name = 'Germany'
 
--- 2.SELECT name, population FROM world
---   WHERE name IN ('Sweden', 'Norway', 'Denmark');
+-- 2.Checking a list The word IN allows us to check if an item is in a list. The example shows the name and population for the countries 'Brazil', 'Russia', 'India' and 'China'.Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+    SELECT name, population FROM world
+    WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
--- 3.SELECT name, area FROM world
---   WHERE area BETWEEN 200000 AND 250000
+-- 3.Which countries are not too small and not too big? BETWEEN allows range checking (range specified is inclusive of boundary values). The example below shows countries with an area of 250,000-300,000 sq. km. Modify it to show the country and the area for countries with an area between 200,000 and 250,000.
+    SELECT name, area FROM world
+    WHERE area BETWEEN 200000 AND 250000
 
 -- 1 SELECT name
 
--- 1.
+-- 1.You can use WHERE name LIKE 'B%' to find the countries that start with "B".The % is a wild-card it can match any characters
+    SELECT name FROM world
+    WHERE name LIKE 'Y%'
 
--- 2.
+-- 2.Find the countries that end with y
+    name FROM world
+    WHERE name LIKE '%Y'
 
--- 3.
+-- 3.Luxembourg has an x - so does one other country. List them both.Find the countries that contain the letter x
+    SELECT name FROM world
+    WHERE name LIKE '%X%'
 
--- 4.
+-- 4.Iceland, Switzerland end with land - but are there others Find the countries that end with land
+    SELECT name FROM world
+    WHERE name LIKE '%land'
 
--- 5.
+-- 5.Columbia starts with a C and ends with ia - there are two more like this.Find the countries that start with C and end with ia
+    SELECT name FROM world
+    WHERE name LIKE 'C%Ia'
 
--- 6.
+-- 6.Greece has a double e - who has a double o? Find the country that has oo in the name
+    SELECT name FROM world
+    WHERE name LIKE '%oo%'
 
--- 7.
+-- 7.Bahamas has three a - who else? Find the countries that have three or more a in the name
+    SELECT name FROM world
+    WHERE name LIKE '%a%a%a%'
 
--- 8.
+-- 8.Find the countries that have "t" as the second character
+    SELECT name FROM world
+    WHERE name LIKE '_t%'
+    ORDER BY name
 
 -- 9.
 
