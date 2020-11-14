25d0af8351c3059c5796c04486923dc781c17d62
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-13 20:08:12 -0500

    [sqlzoo] Completed 'where' section

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -9,7 +9,7 @@ SELECT name, population FROM world
 -- 3.
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000
-  
+
 -- 1 SELECT name
 
 -- 1.
@@ -41,32 +41,59 @@ SELECT name, area FROM world
 
 -- 2 SELECT from World
 -- 1.
-
+SELECT name, continent, population FROM world
 -- 2.
-
+SELECT name FROM world
+WHERE population >= 200000000
 -- 3.
-
+SELECT name, gdp/population
+FROM world
+WHERE population > 200000000
 -- 4.
-
+SELECT name, population/1000000
+FROM world
+WHERE continent = 'South America'
 -- 5.
-
+SELECT name, population
+FROM world
+WHERE name IN ('France', 'Germany', 'Italy')
 -- 6.
-
+SELECT name
+FROM world
+WHERE name LIKE '%United%'
 -- 7.
-
+SELECT name, population, area
+FROM world
+WHERE area > '3000000' OR population > '250000000'
 -- 8.
-
+SELECT name, population, area
+FROM world
+WHERE area > '3000000' XOR population > '250000000'
 -- 9.
-
+SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000,2)
+FROM world
+WHERE continent = 'South America'
 -- 10.
-
+SELECT name, ROUND(gdp/population, -3)
+FROM world
+WHERE gdp >= 1000000000000
 -- 11.
-
+SELECT name, capital
+FROM world
+WHERE LENGTH(name) = LENGTH(capital)
 -- 12.
-
+SELECT name, capital
+FROM world
+WHERE LEFT(name,1) = LEFT(capital,1) && name != capital
 -- 13.
-
-
+SELECT name
+FROM world
+WHERE name LIKE '%a%' &&
+      name LIKE '%e%' &&
+      name LIKE '%i%' &&
+      name LIKE '%o%' &&
+      name LIKE '%u%' &&
+      name NOT LIKE '% %'
 
 -- 3 SELECT from Nobel
 -- 1.
