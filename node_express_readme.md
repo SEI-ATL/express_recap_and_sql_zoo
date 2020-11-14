3cad6f2cdfebaf54d32e20bfe5507f8b6c4816e3
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-14 14:54:47 -0500

    [SQLZOO] Completed 'name' section

--- node_express_readme.md
+++ node_express_readme.md
@@ -1,1 +1,2 @@
 <!-- When you're finished updating your node & express readme, copy-paste it into here for submission! -->
+https://github.com/JJURIZ/GA-Node_Express
--- sqloo_results.sql
+++ sqloo_results.sql
@@ -1,41 +1,67 @@
 -- 0 SELECT basics
 -- 1.
-SELECT population FROM world
-  WHERE name = 'Germany'
+SELECT population 
+FROM world
+WHERE name = 'Germany'
 -- 2.
-SELECT name, population FROM world
-  WHERE name IN ('Sweden', 'Norway', 'Denmark');
-
+SELECT name, population 
+FROM world
+WHERE name IN ('Sweden', 'Norway', 'Denmark');
 -- 3.
-SELECT name, area FROM world
-  WHERE area BETWEEN 200000 AND 250000
+SELECT name, area 
+FROM world
+WHERE area BETWEEN 200000 AND 250000
+
 
 -- 1 SELECT name
-
 -- 1.
-
+SELECT name FROM world
+WHERE name LIKE 'Y%'
 -- 2.
-
+SELECT name FROM world
+WHERE name LIKE '%y'
 -- 3.
-
+SELECT name 
+FROM world
+WHERE name LIKE '%x%'
 -- 4.
-
+SELECT name 
+FROM world
+WHERE name LIKE '%land'
 -- 5.
-
+SELECT name 
+FROM world
+WHERE name LIKE 'C%' && 
+      name LIKE '%ia'
 -- 6.
-
+SELECT name 
+FROM world
+WHERE name LIKE '%oo%'
 -- 7.
-
+SELECT name 
+FROM world
+WHERE name LIKE '%a%%a%%a%'
 -- 8.
-
+SELECT name 
+FROM world
+WHERE name LIKE '_t%'
+ORDER BY name
 -- 9.
-
+SELECT name 
+FROM world
+WHERE name LIKE '%o__o%'
 -- 10.
-
+SELECT name 
+FROM world
+WHERE name LIKE '____'
 -- 11.
-
+SELECT name
+FROM world
+WHERE name LIKE capital
 -- 12.
-
+SELECT name
+FROM world
+WHERE capital LIKE concat(name,'_City')
 -- 13, 14, and 15 have a bug, please skip them
 
 
