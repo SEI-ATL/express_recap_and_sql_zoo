268a5e5a49f92a927a230b2f3694796c24474e67
Andrew McManus <andrewmcmanusmusic@gmail.com>
2020-11-14 13:43:59 -0500

    initial

--- node_express_readme.md
+++ node_express_readme.md
@@ -1,1 +1,90 @@
 <!-- When you're finished updating your node & express readme, copy-paste it into here for submission! -->
+# node-express
+
+Node provides a way of using Javascript as a back-end language, and it allows for connecting files together via dependencies. It requires setting up a package.json file that serves as an entry point for the project.
+
+### Importing packages is like importing a set of preexisting tools specifically designed to perform certain tasks.
+
+```
+npm init
+```
+This creates a package.json file, prompting the user for several different parameters, and creates a set of objects containing those parameters.
+
+```
+npm init -y
+```
+
+This is a shortcut that fills in default parameters for package.json.
+
+If we create two files, ```index.js``` and ```myModule.js```, we can connect them in the following ways. If we add this to ```myModule.js```:
+
+```js
+module.exports.meanGirls = () => "I'm a mouse, duh!"
+```
+...then we can run that function in ```index.js``` by doing this:
+
+```js
+const.myModule = require('./myModule.js');
+mymodule.beBasic();
+```
+If we want to create multiple functions in myModule.js for use in index.js, we can do the following in myModule.js:
+
+```js
+function gretchen() {
+  let quote = "I don't think my father, the inventor of toaster strudel, would appreciate that."
+  return quote;
+}
+
+function trustFall () {
+  let quote = "It's not your fault you're so gap toothed."
+  return quote;
+}
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num2 - num1;
+}
+function addAgain(num1, num2) {
+  return num1 + num2;
+}
+module.exports = {
+  meanGirls,
+  trustFall,
+  gretchen,
+  add,
+  subtract
+};
+```
+...and something like this in ```index.js```:
+
+```js
+const {meanGirls, gretchen, trustFall} = require('/myModule.js');
+console.log(gretchen());
+```
+**Is there a JS6 issue here? I got messages asking me to revise it. After looking at some of these libraries, it does seem like it's interchangeable with the JS5 format.**
+
+If we want to import an external package to use with our existing ```index.js``` file, we install the package in the main directory and make sure that:
+```js
+const variable = require('packagename');
+```
+is included in our ```index.js``` file.
+
+When we add the ```node_modules``` folder to ```.gitignore```, we ensure that every package we're using isn't uploaded in our original repo. As long as the package is installed and appears on the list in the ```package.json``` file, we're good to go.
+
+### Additional topics:
+- setting up an express app
+
+
+- creating routes within an express app
+
+
+- sending text from the server back to the client
+
+
+- sending html to the client using a view template
+
+
+- referencing variables in your view templates
--- sqloo_results.sql
+++ sqloo_results.sql
@@ -1,114 +1,107 @@
 -- 0 SELECT basics
 -- 1.
-
+SELECT population FROM world WHERE name = 'Germany';
 -- 2.
-
+SELECT name, population FROM world WHERE name IN ('Sweden', 'Norway', 'Denmark');
 -- 3.
-
--- 1 SELECT name
-
+SELECT name, area FROM world WHERE area BETWEEN 200000 AND 250000;
+-- 1 SELECT name ???
 -- 1.
-
 -- 2.
-
 -- 3.
-
 -- 4.
-
 -- 5.
-
 -- 6.
-
 -- 7.
-
 -- 8.
-
 -- 9.
-
 -- 10.
-
 -- 11.
-
 -- 12.
-
 -- 13, 14, and 15 have a bug, please skip them
-
 
 -- 2 SELECT from World
 -- 1.
-
+SELECT name, continent, population FROM world;
 -- 2.
-
+SELECT name FROM world WHERE population > 200000000;
 -- 3.
-
+SELECT name, gdp/population FROM world WHERE population > 200000000;
 -- 4.
-
+SELECT name, population/1000000 FROM world WHERE continent = 'South America';
 -- 5.
-
+SELECT name, population FROM world WHERE name= 'France' OR name= 'Germany' OR name= 'Italy';
 -- 6.
-
+SELECT name FROM world WHERE name LIKE 'United%';
 -- 7.
-
+SELECT name, population, area FROM world
+WHERE area > 3000000 OR population > 250000000;
 -- 8.
-
+SELECT name, population, area FROM world
+WHERE area > 3000000 XOR population > 250000000;
 -- 9.
-
+SELECT name, ROUND(population/1000000, 2), ROUND(GDP/1000000000, 2) FROM world
+WHERE continent='South America';
 -- 10.
-
+SELECT name, ROUND(GDP/population, -3) FROM world
+WHERE GDP>1000000000000;
 -- 11.
-
+SELECT name, capital FROM world WHERE LENGTH(name) = LENGTH(capital);
 -- 12.
-
--- 13.
-
-
+SELECT name, capital FROM world WHERE LEFT(name,1) = LEFT(capital,1) AND capital <> name;
 
 -- 3 SELECT from Nobel
 -- 1.
-
+SELECT yr, subject, winner FROM nobel WHERE yr = 1950;
 -- 2.
-
+SELECT winner FROM nobel WHERE yr = 1962 AND subject = 'Literature';
 -- 3.
-
+SELECT yr, subject FROM nobel WHERE winner = 'Albert Einstein';
 -- 4.
-
+SELECT winner FROM nobel WHERE subject = 'Peace' AND yr > 1999;
 -- 5.
-
+SELECT * FROM nobel WHERE subject = 'Literature' AND yr BETWEEN '1980' AND '1989';
 -- 6.
-
+SELECT * FROM nobel WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama');
 -- 7.
-
+SELECT winner FROM nobel WHERE winner LIKE 'John%';
 -- 8.
-
+SELECT * FROM nobel WHERE subject = 'Physics' AND yr = '1980' OR subject = 'Chemistry' AND yr = '1984';
 -- 9.
-
+SELECT * FROM nobel WHERE yr = '1980' AND subject <> 'Chemistry' AND subject <> 'Medicine';
 -- 10.
-
+SELECT * FROM nobel WHERE subject = 'Medicine' AND yr < 1910 OR subject = 'Literature' AND yr >= '2004';
 -- 11.
-
+SELECT * FROM nobel WHERE winner LIKE 'Peter Gr%';
 -- 12.
-
+SELECT * FROM nobel WHERE winner LIKE 'Eugene O%';
 -- 13.
-
+SELECT winner, yr, subject FROM nobel WHERE winner LIKE 'Sir%' ORDER BY yr DESC;
 -- 14.
-
+SELECT winner, subject FROM nobel WHERE yr = 1984 ORDER BY subject IN ('Physics', 'Chemistry'), subject, winner; 
 
 
 -- 4 SELECT within SELECT
 -- 1.
-
+SELECT name FROM world WHERE population > (SELECT population FROM world WHERE name='Russia');
 -- 2.
-
+SELECT name FROM world WHERE continent = 'Europe' AND gdp/population > (SELECT gdp/population FROM world WHERE name = 'United Kingdom');
 -- 3.
-
+SELECT name, continent FROM world WHERE continent = (SELECT continent FROM world WHERE name = 'Argentina') OR continent = (SELECT continent FROM world WHERE name = 'Australia') ORDER BY name
 -- 4.
-
+SELECT name, population FROM world WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world WHERE name = 'Poland');
 -- 5.
+SELECT name, CONCAT(ROUND(population/(SELECT population FROM world WHERE name = 'Germany') * 100, 0), '%') FROM world WHERE continent = 'Europe'; 
 
 -- As noted in the tutorial, questions 6 - 10 in this unit are considered bonuses, just like units 6 - 8+
 -- 6.
-
+SELECT name FROM world WHERE gdp > ALL(SELECT gdp FROM world WHERE continent = 'Europe' AND gdp > 0)
 -- 7.
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0);
 
 -- 8.
 
