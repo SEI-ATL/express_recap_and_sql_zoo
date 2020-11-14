813d19e906084adbfe1c30c96bd4a4e84521e911
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-11-14 16:39:16 -0500

    finished sql-zoo and added to readme

--- node_express_readme.md
+++ node_express_readme.md
@@ -1,1 +1,175 @@
-<!-- When you're finished updating your node & express readme, copy-paste it into here for submission! -->
+# Node-express
+Node express
+## Creating Node
+1. You go to your `Github' and create `Repo`.
+2. Once you create that `Repo` go into your terminals to `clone` it locally.
+3. once you `Clone` your repo
+4. Be sure to go into your `repo folder` to start the process.
+5. Two options you have are `npm intit` which will ask you details on your prohect if you want to ve more descriptive, like...
+```iterm2
+My-first-node-project git:(main) npm init
+This utility will walk you through creating a package.json file.
+It only covers the most common items, and tries to guess sensible defaults.
+
+See `npm help json` for definitive documentation on these fields
+and exactly what they do.
+
+Use `npm install <pkg>` afterwards to install a package and
+save it as a dependency in the package.json file.
+
+Press ^C at any time to quit.
+package name: (my-first-node-project) my-first-node-project
+version: (1.0.0)
+description: "This is my first node project"
+entry point: (index.js) npm init -y
+test command: touch index.html
+git repository: (https://github.com/canourrea23/My-first-node-project.git)
+keywords: [],
+author: Carolina Urrea
+license: (ISC) "ICS",
+Sorry, license should be a valid SPDX license expression (without "LicenseRef"), "UNLICENSED", or "SEE LICENSE IN <filename>".
+license: (ISC)
+About to write to /Users/carolinaurrea/Desktop/SEI1019/unit_two/codealong/My-first-node-project/package.json:
+
+{
+  "name": "my-first-node-project",
+  "version": "1.0.0",
+  "description": "\"This is my first node project\"",
+  "main": "npm init -y",
+  "scripts": {
+    "test": "touch index.html"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/canourrea23/My-first-node-project.git"
+  },
+  "keywords": [
+    "[]"
+  ],
+  "author": "Carolina Urrea",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/canourrea23/My-first-node-project/issues"
+  },
+  "homepage": "https://github.com/canourrea23/My-first-node-project#readme"
+}
+
+
+Is this OK? (yes) yes
+```
+
+6. or `npm init -y` which will defailt all the settings to yes and blank
+
+```
+ My-first-node-project git:(main) ✗ touch index.js
+We added the index.js and module.js
+opened index.js 
+All files are linked from one js file to an other
+
+```javaScript
+const { add, subtract } = require("./myModule");
+
+let name = 'Carolina Urrea'
+console.log(name)
+
+function printName(person) {
+    return `Hello, ${person}`;
+}
+console.log(printName(name));
+
+console.log(add(50, 5));
+console.log(subtract(5, 15));
+```
+
+```javaScript
+const beBasic = () => "That's so fetch!"
+function add(num1, num2){
+    return num1+ num2;
+}
+
+function subtract(num1, num2) {
+    return num2 - num1;
+}
+module.exports = {
+    beBasic,
+    add,
+    subtract,
+}
+```
+
+6. After adding the modules make sure you don't push that to github. you can hide the modules bu creating .gitignore and place the module file
+
+```
+node_modules
+```
+Create a views folder inside your project directory. Inside this folder, create three HTML files:
+index.html (this is the standard filename for the view associated with the base URL)
+about.html
+blog-directory.html
+
+Put some basic HTML in these files so you can test them.
+
+In your routes, replace the res.send(<message>) with res.sendFile(<absolute file path>)
+
+```js
+app.get('/', function(req, res) {
+  res.sendFile(__dirname+'/views/index.html');
+});
+```
+
+Add EJS to your personal website project using npm:
+
+```iTerm
+~ [npm install ejs]
+```
+After you set-up view engine
+
+```js
+app.set('view engine', 'ejs');
+```
+Adapt your routes to ejs
+1. Rename the .html files to .ejs files.
+2. Replace your res.sendFile(<absolute path>) statements with res.render(<file name>) statements.
+3. Ejs assumes a lot about the path to the template files, so as long as they are nested in a views folder and have .ejs extensions, you can simply pass the filename (no extension, though it wont break it if you include it) into res.render().
+
+
+Your home route should look like this:
+```js
+app.get('/', function(req, res) {
+  res.render('index.ejs');
+});
+```
+### index.js
+Templating with variables means we can pass in an object to res.render() and access the values stored in it as variables inside the ejs template.
+This is best demonstrated with an example. Create an object with at least one key-value pair and pass that object in as the second argument to the render function in one of your routes:
+
+```index.js
+app.get('/', function(req, res) {
+  res.render('index', {name: "Sterling Archer", age: 35});
+});
+```
+
+We now have access to a name variable inside our index.ejs file! We can access this variable by embedding it into the html using this notation: <%= embedded js goes here %>.
+For example: 
+```index.ejs
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, <%= name %>!</h1>
+  </body>
+</html>
+```
+### Partails
+
+Partials can be used to modularize views and reduce repetition. A common pattern is to move the header and footer of a page into separate views, or partials, then render them on each page.
+Create the partials
+
+In the main directory of your project, create a partials folder that includes a header.ejs file.
+```partials/header.ejs
+  <header>
+    <img src="http://placekitten.com/500/500">
+  </header>
+  ```
--- sqloo_results.sql
+++ sqloo_results.sql
@@ -1,192 +1,393 @@
 -- 0 SELECT basics
 -- 1.
-
+SELECT population FROM world
+  WHERE name = 'Germany'
 -- 2.
-
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
 -- 3.
-
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
 -- 1 SELECT name
-
 -- 1.
-
+SELECT name FROM world
+  WHERE name LIKE 'Y%'
 -- 2.
-
+SELECT name FROM world
+  WHERE name LIKE '%y'
 -- 3.
-
+SELECT name FROM world
+  WHERE name LIKE '%x%'
 -- 4.
-
+SELECT name FROM world
+  WHERE name LIKE '%land'
 -- 5.
-
+SELECT name FROM world
+  WHERE name LIKE 'C%ia'
 -- 6.
-
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
 -- 7.
-
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
 -- 8.
-
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
 -- 9.
-
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
 -- 10.
-
+SELECT name FROM world
+ WHERE name LIKE '____'
 -- 11.
-
+SELECT name
+  FROM world
+ WHERE capital = name;
 -- 12.
-
+SELECT name, capital
+FROM world
+WHERE name <> capital and LEFT(name, 1) = LEFT(capital, 1)
 -- 13, 14, and 15 have a bug, please skip them
 
 
 -- 2 SELECT from World
 -- 1.
-
+SELECT name, continent, population FROM world
 -- 2.
-
+SELECT name FROM world
+WHERE population > 200000000
 -- 3.
-
+SELECT name, gdp/population
+FROM world
+WHERE population > 200000000
 -- 4.
-
+SELECT name, population/1000000
+FROM world 
+WHERE continent = 'South America'
 -- 5.
-
+SELECT name, population
+FROM world
+WHERE name IN ('France','Germany','Italy')
 -- 6.
-
+SELECT name 
+FROM world
+WHERE name LIKE '%United%'
 -- 7.
-
+SELECT name, population, area
+FROM world
+WHERE population > 250000000
+OR area > 3000000
 -- 8.
+SELECT name, population, area
+FROM world 
+WHERE population > 250000000
+XOR area > 3000000
 
 -- 9.
-
+select name, ROUND(population/1000000,2), ROUND(gdp/1000000000,2) from world
+  where continent = 'South America'
 -- 10.
-
+SELECT name, ROUND(gdp/population, -3)
+FROM world
+WHERE gdp > 1000000000000
 -- 11.
-
+SELECT name, capital
+FROM world
+WHERE LENGTH(name) = LENGTH(capital)
 -- 12.
-
+SELECT name, capital
+FROM world
+WHERE name <> capital and LEFT(name, 1) = LEFT(capital, 1)
 -- 13.
-
-
-
+SELECT name
+FROM world
+WHERE name LIKE '%a%' 
+AND name LIKE '%e%'
+AND name LIKE '%i%'
+AND name LIKE '%o%'
+AND name LIKE '%u%'
+AND POSITION(' ' in name) = 0
 -- 3 SELECT from Nobel
 -- 1.
-
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
 -- 2.
-
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
 -- 3.
-
+SELECT yr, subject
+FROM nobel 
+WHERE winner = 'Albert Einstein'
 -- 4.
-
+SELECT winner
+FROM nobel
+WHERE yr >= 2000
+AND subject = 'Peace'
 -- 5.
-
+SELECT yr, subject, winner
+FROM nobel 
+WHERE subject = 'Literature'
+AND yr BETWEEN 1980 AND 1989
 -- 6.
-
+SELECT * FROM nobel
+WHERE winner IN('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama')
 -- 7.
-
+SELECT winner
+FROM nobel
+WHERE winner LIKE 'John%'
 -- 8.
-
+SELECT yr, subject, winner
+FROM nobel 
+WHERE (subject = 'Physics'
+AND yr = 1980)
+OR (subject = 'Chemistry'
+AND yr = 1984)
 -- 9.
-
+SELECT yr, subject, winner
+FROM nobel 
+WHERE yr = 1980
+AND not subject in ('Chemistry', 'Medicine')
 -- 10.
-
+SELECT yr, subject, winner
+FROM nobel 
+WHERE (subject = 'Medicine'
+AND yr < 1910)
+OR (subject = 'Literature' 
+AND yr >= 2004)
 -- 11.
-
+SELECT yr, subject, winner
+FROM nobel
+WHERE winner = 'PETER GRÜNBERG'
 -- 12.
-
+SELECT yr, subject, winner
+FROM nobel
+WHERE winner = 'EUGENE O''NEILL'
 -- 13.
-
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'Sir%'
 -- 14.
-
-
-
+SELECT winner, subject
+FROM nobel
+WHERE yr=1984
+ORDER BY subject IN ('Physics','Chemistry'), subject,winner
 -- 4 SELECT within SELECT
 -- 1.
-
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
 -- 2.
-
+SELECT name 
+FROM world
+WHERE continent = 'Europe' 
+AND gdp/population > (SELECT
+     gdp/population FROM world
+      WHERE name = 'United Kingdom')
 -- 3.
-
+SELECT name, continent FROM world
+WHERE continent IN (SELECT continent FROM world
+WHERE name IN (‘Argentina’,‘Australia’))
+ORDER BY name
 -- 4.
-
+SELECT name, population
+FROM world
+WHERE population > 'Canada'
+AND population < 'Poland'
 -- 5.
-
+SELECT name, CONCAT(ROUND(population/(SELECT population FROM world
+                          WHERE name = 'Germany')*100,0),'%')
+             FROM world WHERE continent = 'Europe'
 -- As noted in the tutorial, questions 6 - 10 in this unit are considered bonuses, just like units 6 - 8+
 -- 6.
-
+SELECT name 
+FROM world
+WHERE gdp > ALL(SELECT gdp FROM world
+                   WHERE gdp > 0 AND continent = 'Europe')
 -- 7.
-
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area > 0)
 -- 8.
-
+SELECT continent, name FROM world x
+  WHERE name <= ALL
+    (SELECT name FROM world y
+        WHERE y.continent=x.continent)
 -- 9.
-
+SELECT name, continent, population FROM world x
+  WHERE 25000000 >= ALL(SELECT population
+	                FROM world y
+		        WHERE x.continent = y.continent
+                        AND y.population > 0);
 -- 10.
-
+SELECT name, continent FROM world x
+  WHERE population >= ALL(SELECT population*3
+                         FROM world y
+                         WHERE x.continent = y.continent
+                         and y.name != x.name)
 
 
 -- 5 SUM and COUNT
 -- 1.
-
+SELECT SUM(population)
+FROM world
 -- 2.
-
+SELECT DISTINCT continent 
+FROM world
 -- 3.
-
+SELECT SUM(gdp)
+FROM world 
+WHERE continent = 'Africa'
 -- 4.
-
+SELECT COUNT(name) 
+FROM world
+WHERE area >= 1000000
 -- 5.
-
+SELECT SUM(population) 
+FROM world
+WHERE name IN ('Estonia','Latvia','Lithuania')
 -- 6.
-
+SELECT continent, COUNT(name) 
+FROM world
+GROUP BY continent
 -- 7.
-
+SELECT continent, COUNT(name) 
+FROM world
+WHERE population > 10000000
+GROUP BY continent
 -- 8.
-
+SELECT continent 
+FROM world
+GROUP BY continent
+HAVING SUM(population) > 100000000
 
 -- Note: the units below this are bonus for this weekend, and they will be required in a future assignment. If you do them now you will be ahead of the game!
 -- 6 JOIN
 -- 1.
-
+SELECT matchid, player
+FROM goal 
+WHERE teamid = 'GER'
 -- 2.
-
+SELECT id,stadium,team1,team2
+FROM game
+WHERE id = '1012'
 -- 3.
-
+SELECT player, teamid, stadium, mdate
+FROM game
+JOIN goal ON (id=matchid)
+WHERE teamid = 'Ger'
 -- 4.
-
+SELECT team1, team2, player  
+FROM game
+JOIN goal ON (id=matchid)
+WHERE player LIKE 'Mario%'
 -- 5.
-
+SELECT player, teamid, coach, gtime
+FROM goal 
+JOIN eteam ON (teamid=id)
+WHERE gtime<=10
 -- 6.
-
+SELECT mdate, teamname 
+FROM game
+JOIN eteam ON (team1 = eteam.id)
+WHERE coach = 'Fernando Santos'
 -- 7.
-
+SELECT player 
+FROM goal
+JOIN game ON (matchid = id)
+WHERE stadium = 'National Stadium, Warsaw'
 -- 8.
-
+SELECT DISTINCT player
+FROM game JOIN goal ON matchid = id
+WHERE (team1= 'GER' OR team2='GER')
+AND teamid != 'GER'
 -- 9.
-
+SELECT teamname, COUNT(*)
+FROM eteam JOIN goal ON id=teamid
+GROUP BY teamname
 -- 10.
-
+SELECT stadium, COUNT(*) 
+FROM goal
+JOIN game ON (matchid = id)
+GROUP BY stadium
 -- 11.
-
+SELECT matchid, mdate, COUNT(teamid)
+FROM game 
+JOIN goal ON matchid = id
+WHERE (team1 = 'POL' OR team2 = 'POL')
+GROUP BY mdate,matchid
 -- 12.
-
+SELECT matchid, mdate, COUNT(*)  
+FROM goal
+JOIN game ON (matchid=id)
+WHERE teamid = 'GER'
+GROUP BY matchid, mdate
 -- 13.
-
+SELECT DISTINCT mdate, team1, SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1,
+    team2, SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2
+FROM game
+LEFT JOIN goal ON game.id = goal.matchid
+GROUP BY id, mdate, team1, team2
+ORDER BY mdate, matchid, team1, team2
 
 
 -- 7 More JOIN operations
 -- 1.
-
+SELECT id, title
+ FROM movie
+ WHERE yr=1962
 -- 2.
-
+SELECT yr
+FROM movie
+WHERE title = 'Citizen Kane'
 -- 3.
-
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE 'Star Trek%'
+ORDER BY yr
 -- 4.
-
+SELECT id 
+FROM actor
+WHERE name = 'Glenn Close'
 -- 5.
-
+SELECT id 
+FROM movie
+WHERE title = 'Casablanca'
 -- 6.
-
+SELECT name 
+FROM casting 
+JOIN actor ON (id = actorid)
+WHERE movieid = 11768
 -- 7.
-
+SELECT name 
+FROM casting
+JOIN actor ON (actor.id=actorid)
+JOIN movie ON (movie.id=movieid)
+WHERE title = 'Alien'
 -- 8.
-
+SELECT title 
+FROM casting
+JOIN movie ON (movie.id = movieid)
+JOIN actor ON (actor.id = actorid)
+WHERE name = 'Harrison Ford'
 -- 9.
-
+SELECT title 
+FROM casting
+JOIN movie ON (movie.id = movieid)
+JOIN actor ON (actor.id = actorid)
+WHERE name = 'Harrison Ford'  AND ord > 1
 -- 10.
-
+SELECT title, name 
+FROM casting
+JOIN movie ON (movie.id = movieid)
+JOIN actor ON (actor.id = actorid)
+WHERE yr = 1962 and ord = 1
 -- 11.
 
 -- 12.
