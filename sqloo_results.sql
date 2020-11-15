d9905098e961381e41d5d3a5e5d013d4506e4dc4
Jenny <finej01@gmail.com>
2020-11-14 22:45:47 -0500

    update join pt2 answers

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -307,56 +307,134 @@ SELECT continent
 -- Note: the units below this are bonus for this weekend, and they will be required in a future assignment. If you do them now you will be ahead of the game!
 -- 6 JOIN
 -- 1.
-
+SELECT matchid, player
+    FROM goal
+    WHERE teamid = 'GER';
 -- 2.
-
+SELECT id, stadium, team1, team2
+    FROM game
+    WHERE id = 1012;
 -- 3.
-
+SELECT player,teamid, stadium, mdate
+  FROM game
+    INNER JOIN goal ON game.id=goal.matchid
+    WHERE teamid = 'GER';
 -- 4.
-
+SELECT team1, team2, player
+    FROM game
+    INNER JOIN goal ON game.id = goal.matchid
+    WHERE player LIKE 'Mario%';
 -- 5.
-
+SELECT player, teamid, coach, gtime
+    FROM goal
+    INNER JOIN eteam ON goal.teamid = eteam.id
+    WHERE gtime <= 10;
 -- 6.
-
+SELECT mdate, teamname
+    FROM game
+    INNER JOIN eteam ON game.team1 = eteam.id
+    WHERE coach ='Fernando Santos';
 -- 7.
-
+SELECT player
+    FROM goal
+    INNER JOIN game ON goal.matchid = game.id
+    WHERE stadium = 'National Stadium, Warsaw';
 -- 8.
-
+SELECT DISTINCT(player)
+    FROM game
+    JOIN goal ON game.id = goal.matchid
+    WHERE ((team1='GER' OR team2='GER') AND teamid != 'GER');
 -- 9.
-
+SELECT teamname, COUNT(player)
+    FROM eteam
+    JOIN goal ON eteam.id=goal.teamid
+    GROUP BY teamname;
 -- 10.
-
+SELECT stadium, COUNT(player)
+    FROM goal
+    INNER JOIN game ON goal.matchid = game.id
+    GROUP BY stadium;
 -- 11.
-
+SELECT id, mdate, COUNT(player)
+    FROM game
+    LEFT JOIN goal ON goal.matchid = game.id
+    WHERE team1 = 'POL' OR team2 = 'POL'
+    GROUP BY id, mdate;
 -- 12.
-
+SELECT matchid, mdate, COUNT(player)
+    FROM game
+    INNER JOIN goal ON game.id = goal.matchid
+    WHERE teamid = 'GER'
+    GROUP BY matchid, mdate;
 -- 13.
-
-
+SELECT mdate, team1,
+    SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1,
+    team2,
+    SUM(CASE WHEN teamid = team2 THEN 1 ELSE 0 END) score2
+    FROM game
+    LEFT JOIN goal ON matchid = id
+    GROUP BY id, mdate, team1, team2
+    ORDER BY mdate, matchid, team1, team2;
 
 -- 7 More JOIN operations
 -- 1.
-
+SELECT id, title
+    FROM movie
+    WHERE yr=1962;
 -- 2.
-
+SELECT yr
+FROM movie
+WHERE title = 'Citizen Kane';
 -- 3.
-
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE '%Star Trek%';
+ORDER BY yr;
 -- 4.
-
+SELECT id
+FROM actor
+WHERE name = 'Glenn Close';
 -- 5.
-
+SELECT id
+FROM movie
+WHERE title = 'Casablanca';
 -- 6.
-
+SELECT name
+FROM actor
+INNER JOIN casting ON actor.id = casting.actorid
+WHERE movieid = (SELECT id
+    FROM movie
+    WHERE title = 'Casablanca');
 -- 7.
-
+SELECT name
+FROM actor
+INNER JOIN casting ON actor.id = casting.actorid
+WHERE movieid = (SELECT id
+    FROM movie
+    WHERE title = 'Alien');
 -- 8.
-
+SELECT title
+FROM movie
+INNER JOIN casting ON movie.id = casting.movieid
+WHERE actorid = (SELECT id FROM actor WHERE name = 'Harrison Ford')
 -- 9.
-
+SELECT title
+FROM movie
+INNER JOIN casting ON movie.id = casting.movieid
+WHERE actorid = (SELECT id FROM actor WHERE name = 'Harrison Ford') AND ord !=1;
 -- 10.
-
+SELECT title,name
+FROM movie
+INNER JOIN casting ON casting.movieid = movie.id
+INNER JOIN actor ON actor.id = casting.actorid
+WHERE yr = 1962 AND ord = 1;
 -- 11.
-
+SELECT yr, COUNT(title) FROM
+  movie JOIN casting ON movie.id=movieid
+        JOIN actor   ON actorid=actor.id
+WHERE name='Rock Hudson'
+GROUP BY yr
+HAVING COUNT(title) > 2;
 -- 12.
 
 -- 13.
