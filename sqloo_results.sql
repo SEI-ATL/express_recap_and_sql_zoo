e5874d2eb83f22f63c2b22c7debca2168dffa734
delayedaa <delayedaa@gmail.com>
2020-11-15 17:57:43 -0600

    [sqloo_results.sql] Add answers to unit 7

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -552,17 +552,98 @@ ORDER BY mdate,
 
 -- 7 More JOIN operations
 -- 1.
+SELECT id,
+  title
+FROM movie
+WHERE yr = 1962;
+
 -- 2.
+SELECT yr
+FROM movie
+WHERE title = 'Citizen Kane';
+
 -- 3.
+SELECT id,
+  title,
+  yr
+FROM movie
+WHERE title LIKE '%Star Trek%'
+ORDER BY yr;
+
 -- 4.
+SELECT id
+FROM actor
+WHERE name = 'Glenn Close';
+
 -- 5.
+SELECT id
+FROM movie
+WHERE title = 'Casablanca';
+
 -- 6.
+SELECT name
+FROM actor
+  JOIN casting ON actor.id = actorid
+  JOIN movie ON movieid = movie.id
+WHERE title = 'Casablanca';
+
 -- 7.
+SELECT name
+FROM actor
+  JOIN casting ON actor.id = actorid
+  JOIN movie ON movieid = movie.id
+WHERE title = 'Alien';
+
 -- 8.
+SELECT title
+FROM movie
+  JOIN casting ON movie.id = movieid
+  JOIN actor ON actorid = actor.id
+WHERE name = 'Harrison Ford';
+
 -- 9.
+SELECT title
+FROM movie
+  JOIN casting ON movie.id = movieid
+  JOIN actor ON actorid = actor.id
+WHERE name = 'Harrison Ford'
+  AND ord <> 1;
+
 -- 10.
+SELECT title,
+  name
+FROM movie
+  JOIN casting ON movie.id = movieid
+  JOIN actor ON actorid = actor.id
+WHERE yr = 1962
+  AND ord = 1;
+
 -- 11.
+SELECT yr,
+  COUNT(title)
+FROM movie
+  JOIN casting ON movie.id = movieid
+  JOIN actor ON actorid = actor.id
+WHERE name = 'Rock Hudson'
+GROUP BY yr
+HAVING COUNT(title) > 2;
+
 -- 12.
+SELECT title, name
+FROM casting
+  JOIN movie ON movieid = movie.id
+  JOIN actor ON actorid = actor.id
+WHERE ord = 1
+  AND movie.id IN (
+    SELECT movieid
+    FROM casting
+    WHERE actorid IN (
+      SELECT id
+      FROM actor
+      WHERE name = 'Julie Andrews'
+    )
+  )
+
 -- 13.
 -- 14.
 -- 15.
