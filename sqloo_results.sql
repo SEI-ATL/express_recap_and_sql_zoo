9665ddea92f7d5425f89b872b1a21aec7da77b10
Andrew McManus <andrewmcmanusmusic@gmail.com>
2020-11-14 21:15:18 -0500

    through part 5

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -104,30 +104,32 @@ SELECT continent, name, area FROM world x
           AND area>0);
 
 -- 8.
-
+SELECT continent, name FROM world x 
+WHERE name <= ALL(SELECT name FROM world y 
+WHERE x.continent=y.continent AND name IS NOT NULL);
 -- 9.
-
+SELECT name, continent, population FROM world WHERE continent <> ALL(SELECT continent FROM world WHERE population >= 25000000)
 -- 10.
 
 
 
 -- 5 SUM and COUNT
 -- 1.
-
+SELECT SUM(population) FROM world
 -- 2.
-
+SELECT DISTINCT(continent) FROM world
 -- 3.
-
+SELECT SUM(gdp) FROM world WHERE continent = 'Africa'
 -- 4.
-
+SELECT COUNT(name) FROM world WHERE area > 1000000
 -- 5.
-
+SELECT SUM(population) FROM world WHERE name IN ('Estonia', 'Latvia', 'Lithuania')
 -- 6.
-
+SELECT continent, COUNT(name) FROM world GROUP BY continent
 -- 7.
-
+SELECT continent, COUNT(name) FROM world WHERE population >= 10000000 GROUP BY continent
 -- 8.
-
+SELECT continent FROM world GROUP BY continent HAVING SUM(population) > 100000000
 
 -- Note: the units below this are bonus for this weekend, and they will be required in a future assignment. If you do them now you will be ahead of the game!
 -- 6 JOIN
