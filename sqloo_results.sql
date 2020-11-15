ace96b560a8626ca5ada57e0caef555825de1f20
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-11-14 21:41:13 -0500

    [having computer issuses saving what I have for now]

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -131,21 +131,47 @@ SELECT name
 
 
 -- 3 SELECT from Nobel
--- 1.
+-- 1.Change the query shown so that it displays Nobel prizes for 1950.
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
 
--- 2.
+-- 2.Show who won the 1962 prize for Literature.
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
 
--- 3.
+-- 3.Show the year and subject that won 'Albert Einstein' his prize.
+SELECT yr, subject
+  FROM nobel
+ WHERE winner = 'Albert Einstein' 
 
--- 4.
+-- 4.Give the name of the 'Peace' winners since the year 2000, including 2000.
+SELECT winner
+FROM nobel
+WHERE yr > 1999
+and subject = 'Peace'
 
--- 5.
+-- 5.Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
 
--- 6.
+SELECT yr, subject, winner 
+FROM nobel
+WHERE yr >1979 and yr < 1990
+and subject = 'Literature'
 
--- 7.
+-- 6.Show all details of the presidential winners:Theodore Roosevelt,Woodrow Wilson,Jimmy Carter, Barack Obama
+SELECT * FROM nobel
+ WHERE subject = 'Peace'
+  AND winner IN ('Barack Obama','Jimmy Carter','Woodrow Wilson','Theodore Roosevelt')
 
--- 8.
+
+-- 7.Show the winners with first name John
+SELECT winner 
+FROM nobel
+WHERE winner LIKE 'John%'
+    
+-- 8. Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
 
 -- 9.
 
