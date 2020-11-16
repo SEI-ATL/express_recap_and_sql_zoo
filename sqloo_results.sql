52c605bc1247687243c15fbc3324c1801ed8a55d
jtreeves <jackson.t.reeves@gmail.com>
2020-11-16 04:43:54 -0500

    [sql] Finish required parts of question 4

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -168,22 +168,47 @@ ORDER BY subject, winner
 
 -- 4 SELECT within SELECT
 -- 1.
-
+SELECT name
+FROM world
+WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
 -- 2.
-
+SELECT name
+FROM world
+WHERE continent = 'Europe'
+AND gdp/population > (SELECT gdp/population FROM world WHERE name = 'United Kingdom')
 -- 3.
-
+SELECT name, continent
+FROM world
+WHERE continent = (SELECT continent FROM world WHERE name = 'Argentina')
+OR continent = (SELECT continent FROM world WHERE name = 'Australia')
+ORDER BY name
 -- 4.
-
+SELECT name, population
+FROM world
+WHERE population > (SELECT population FROM world WHERE name = 'Canada')
+AND population < (SELECT population FROM world WHERE name = 'Poland')
 -- 5.
-
+SELECT name, CONCAT(ROUND((population / (SELECT population FROM world WHERE name = 'Germany') * 100), 0), '%') AS percentage
+FROM world
+WHERE continent = 'Europe'
 -- As noted in the tutorial, questions 6 - 10 in this unit are considered bonuses, just like units 6 - 8+
 -- 6.
-
+SELECT name
+FROM world
+WHERE gdp > ALL (SELECT gdp FROM world WHERE continent = 'Europe' AND gdp > 0)
 -- 7.
-
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent = x.continent
+          AND area > 0)
 -- 8.
-
+SELECT continent, name FROM world x
+WHERE name <= ALL
+(SELECT name FROM world y
+WHERE y.continent = x.continent)
 -- 9.
 
 -- 10.
@@ -207,7 +232,7 @@ ORDER BY subject, winner
 -- 8.
 
 
--- Note: the units below this are bonus for this weekend, and they will be required in a future assignment. If you do them now you will be ahead of the game!
+-- Note: the units below this are bonus for this weekend, but they will be required in a future assignment. If you do them now you will be ahead of the game!
 
 -- 6 JOIN
 -- 1.
