59dc9d45f8409430ef3e29784e05de6770002ff4
Edree Huss <edreeshuss@gmail.com>
2020-11-16 08:58:39 -0500

    [Deliverable] Complete questions 1-5.

--- node_express_readme.md
+++ node_express_readme.md
@@ -1,1 +1,121 @@
 <!-- When you're finished updating your node & express readme, copy-paste it into here for submission! -->
+
+# Node + Express + EJS Instructions
+## [Node](#Node)<br>
+## [Express](#Express)<br>
+## [EJS](#EJS)<br>
+
+
+<br>
+
+## Node
+
+1. Create a repo and `clone` to local machine.
+```bash
+git clone https://github.com/edgerees/hello-express.git
+```
+2. Open the the file location using your terminal.
+
+3. Create your JS file which will be called `index.js`
+```bash
+touch index.js
+```
+4. In terminal, whichin the project folder, initialize the node command.
+```bash
+npm init
+```
+
+5. Give the package name, ours will be `node_practice`.
+
+6. Since it is the 1st version it will be listed as 
+```bash
+1.0.0
+```
+7. Click `enter` when it states the license and click `Yes` when it asks if it is OK.
+
+8. Now execute the JS file using a NPM command in terminal.
+
+In the package.json file created, add the following code:
+```json
+"start" : "node index.js"
+```
+Then run the node start command in the terminal.
+```bash
+$ node start
+(this will run the code you have written in the js file.)
+```
+9. Add someones interesting NPM package. Find an interesing NPM pacakge
+
+[https://www.npmjs.com/package/@ultraq/array-utils]
+
+10.  Run the insall code in your terminal 
+```bash
+npm i @ultraq/array-utils
+```
+11. Now add a `.gitignore` in the Node Modules folder.
+        1. Open terminal and go to the node_modules fodler.
+        2. Run the following code.
+            ```bash
+                npm i create-gitignore
+            ```
+12. `Commit` and `Push` to GitHub!
+```bash
+git add .
+git commit -m'[Node_Modules]Setup node modules.'
+git push
+```
+
+## Express
+1. Create a repo and `clone` to local machine.
+```bash
+git clone https://github.com/edgerees/hello-express.git
+```
+2. Type `npm i express` in terminal to install express
+```bash
+npm i express
+```
+3. Create a app.js
+```bash
+touch app.js
+```
+4. Add code to `app.js` to import the express module and also create an express application
+```js
+const express = require('express');
+const app = express();
+```
+5. Insert code to `app.js` to create a home route. 
+```js
+app.get('/', function(req, res) {
+    res.send('Hello, World!');
+```
+6. Now tell the application to listen to the specified port. Type in `app.js`..
+```js
+app.listen(8000);
+```
+7. Type `nodemon index.js` in terminal to run program in server.
+
+8. Open your internet browser and type `localhost:8000` in url bar to view your application. 
+
+
+## EJS:
+1. Open terminal and type the following in file location to install ejs
+```bash
+npm install ejs
+```
+2. Open `app.js` and enter in the following before routes.
+```js
+app.set('view engine', 'ejs');
+```
+3. Place the .html files in a folder you made named `views` and rename the file type to `.ejs` instead of `.html`
+
+4. Change route to:
+```js
+app.get('/(filename)', function(req, res) {
+  res.render('(filename)', object);
+});
+```
+5. The render function can take in an object as a parameter and access the values stored in it as variables inside the ejs template
+6. The correct syntax for emedding javascript code into the ejs files is written as...
+```js
+<%= (embedded js goes here) %>
+```
--- sqloo_results.sql
+++ sqloo_results.sql
@@ -1,139 +1,226 @@
 -- 0 SELECT basics
--- 1.
 
--- 2.
+- 1. SELECT population FROM world
+        WHERE name = 'Germany'
 
--- 3.
+- 2. SELECT name, population FROM world
+        WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+- 3. SELECT name, area FROM world
+         WHERE area BETWEEN 200000 AND 250000
 
 -- 1 SELECT name
 
--- 1.
+- 1. SELECT name FROM world
+        WHERE name LIKE 'Y%'
 
--- 2.
+- 2. SELECT name FROM world
+        WHERE name LIKE '%Y'
 
--- 3.
+- 3. SELECT name FROM world
+        WHERE name LIKE '%x%'
 
--- 4.
+- 4. SELECT name FROM world
+        WHERE name LIKE '%land'
 
--- 5.
+- 5. SELECT name FROM world
+        WHERE name LIKE 'c%ia'
 
--- 6.
+- 6. SELECT name FROM world
+        WHERE name LIKE '%oo%'
 
--- 7.
+- 7. SELECT name FROM world
+        WHERE name LIKE '%a%a%a%'
 
--- 8.
+- 8. SELECT name FROM world
+        WHERE name LIKE '_t%'
+        ORDER BY name
 
--- 9.
+- 9. SELECT name FROM world
+        WHERE name LIKE '%o__o%'
 
--- 10.
+- 10. SELECT name FROM world
+        WHERE name LIKE '____'
 
--- 11.
+- 11. SELECT name
+        FROM world
+        WHERE name = capital
 
--- 12.
+- 12. SELECT name FROM world
+        WHERE capital = concat(name, ' city');
 
--- 13, 14, and 15 have a bug, please skip them
+- 13, 14, and 15 have a bug, please skip them
 
 
 -- 2 SELECT from World
--- 1.
 
--- 2.
+- 1. SELECT name, continent, population FROM world
 
--- 3.
+- 2. SELECT name FROM world
+        WHERE population >= 200000000;
 
--- 4.
+- 3. SELECT name, gdp/population FROM world
+        WHERE population >= 200000000;
 
--- 5.
+- 4. SELECT name, population/1000000 FROM world
+        WHERE continent = 'South America';
 
--- 6.
+- 5. SELECT name, population FROM world
+        WHERE name IN ('France', 'Germany', 'Italy');
 
--- 7.
+- 6. SELECT name FROM world
+        WHERE name LIKE 'United %';
 
--- 8.
+- 7. SELECT name, population, area FROM world
+         WHERE area > 3000000 OR population > 250000000;
 
--- 9.
+- 8. SELECT name, population, area FROM world
+        WHERE area > 3000000 XOR population > 250000000;
 
--- 10.
+- 9. SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) FROM world
+        WHERE continent = 'South America';
 
--- 11.
+- 10. SELECT name, ROUND(GDP/population, -3) FROM world
+        WHERE gdp >= 1000000000000;
 
--- 12.
+- 11. SELECT name, capital FROM world
+        WHERE LENGTH(name) = LENGTH(capital);
 
--- 13.
+- 12. SELECT name, capital FROM world
+        WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital;
+
+- 13. SELECT name FROM world
+        WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name NOT LIKE '% %';
+
 
 
 
 -- 3 SELECT from Nobel
--- 1.
 
--- 2.
+- 1. SELECT yr, subject, winner
+        FROM nobel
+        WHERE yr = 1950
 
--- 3.
+- 2. SELECT winner FROM nobel
+        WHERE yr = 1962 AND subject = 'Literature'
 
--- 4.
+- 3. SELECT yr, subject FROM nobel
+        WHERE winner = 'Albert Einstein';
 
--- 5.
+- 4. SELECT winner FROM nobel
+        WHERE yr >= 2000 AND subject = 'Peace';
 
--- 6.
+- 5. SELECT yr, subject, winner FROM nobel
+        WHERE yr <= 1989 AND yr >= 1980 AND subject = 'Literature';
 
--- 7.
+- 6. SELECT * FROM nobel
+        WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama');
 
--- 8.
+- 7. SELECT winner FROM nobel
+        WHERE winner LIKE 'John %';
 
--- 9.
+- 8. SELECT yr, subject, winner FROM nobel
+        WHERE (subject = 'physics' AND yr = 1980) OR (subject = 'chemistry' AND yr = 1984);
 
--- 10.
+- 9. SELECT yr, subject, winner FROM nobel
+        WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine');
 
--- 11.
+- 10. SELECT yr, subject, winner FROM nobel
+        WHERE (subject = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004);
 
--- 12.
+- 11. SELECT * FROM nobel
+        WHERE winner = 'PETER GRÜNBERG';
 
--- 13.
+- 12. SELECT * FROM nobel
+        WHERE winner = 'EUGENE O''NEILL';
 
--- 14.
+- 13. SELECT winner, yr, subject FROM nobel
+        WHERE winner LIKE 'Sir %'
+        ORDER BY yr DESC, winner;
 
+- 14. SELECT winner, subject FROM nobel
+        WHERE yr = 1984
+        ORDER BY subject IN ('Physics', 'Chemistry'), subject, winner;
 
 
 -- 4 SELECT within SELECT
--- 1.
 
--- 2.
+- 1. SELECT name FROM world
+        WHERE population >
+            (SELECT population FROM world
+            WHERE name = 'Russia');
 
--- 3.
+- 2. SELECT name FROM world
+        WHERE continent = 'Europe' AND GDP/population >
+            (SELECT GDP/population FROM world
+            WHERE name = 'United Kingdom');
 
--- 4.
+- 3. SELECT name, continent FROM world
+        WHERE continent IN 
+            (SELECT continent FROM world
+            WHERE name IN ('Australia', 'Argentina'))
+        ORDER BY name;
 
--- 5.
+- 4. SELECT name FROM world
+        WHERE population > 
+            (SELECT population FROM world WHERE name = 'Canada')
+        AND population <
+            (SELECT population FROM world WHERE name = 'Poland');
 
--- As noted in the tutorial, questions 6 - 10 in this unit are considered bonuses, just like units 6 - 8+
--- 6.
-
--- 7.
-
--- 8.
-
--- 9.
-
--- 10.
+- 5. SELECT name, CONCAT(ROUND(100 * population/(SELECT population FROM world 
+        WHERE name = 'Germany')), '%')
+        FROM world
+        WHERE continent = 'Europe';
 
 
 
--- 5 SUM and COUNT
--- 1.
 
--- 2.
 
--- 3.
+-- - As noted in the tutorial, questions 6 - 10 in this unit are considered bonuses, just like units 6 - 8+
+-- - 6.
 
--- 4.
+-- - 7.
 
--- 5.
+-- - 8.
 
--- 6.
+-- - 9.
 
--- 7.
+-- - 10.
 
--- 8.
+
+
+- 5 SUM and COUNT
+
+- 1. SELECT SUM(population)
+        FROM world
+
+- 2. SELECT DISTINCT continent FROM world
+
+- 3. SELECT SUM(gdp)
+        FROM world
+        WHERE continent = 'Africa'
+
+- 4. SELECT COUNT(name)
+        FROM world
+        WHERE area >= '1000000'
+
+- 5. SELECT SUM(population)
+        FROM world
+        WHERE name IN ('Estonia', 'Latvia', 'Lithuania')
+
+- 6. SELECT continent, COUNT(name)
+        FROM world
+        GROUP BY continent
+
+- 7. SELECT continent, COUNT(name)
+        FROM world
+        WHERE population >= 10000000
+        GROUP BY continent
+
+- 8. SELECT continent
+        FROM world
+        GROUP BY continent
+        HAVING SUM(population) >= 100000000
 
 
 -- Note: the units below this are bonus for this weekend, and they will be required in a future assignment. If you do them now you will be ahead of the game!
