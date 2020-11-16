18ab69d2d6fb4eda3959dd15eb6cb4d55c859f5c
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-11-16 08:59:37 -0500

    Began bonuses

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -356,16 +356,45 @@ FROM (SELECT * FROM world a WHERE population > 10000000) b GROUP BY continent;
 -- Note: the units below this are bonus for this weekend, and they will be required in a future assignment. If you do them now you will be ahead of the game!
 -- 6 JOIN
 -- 1.
+show the matchid and player name for all goals scored by Germany
+
+SELECT matchid, player FROM goal 
+  WHERE teamid = 'GER';
 
 -- 2.
+Show id, stadium, team1, team2 for just game 1012
+
+SELECT id,stadium,team1,team2
+  FROM game
+WHERE id = 1012;
 
 -- 3.
+show the player, teamid, stadium and mdate for every German goal
+
+SELECT player, teamid, stadium, mdate
+  FROM game JOIN goal ON (id=matchid)
+WHERE teamid = 'GER';
 
 -- 4.
+Show the team1, team2 and player for every goal scored by a player called Mario
+
+SELECT team1, team2, player
+FROM game JOIN goal ON (id=matchid)
+WHERE player LIKE 'Mario%';
 
 -- 5.
+Show player, teamid, coach, gtime for all goals scored in the first 10 minutes
+
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam ON teamid=id
+ WHERE gtime<=10;
 
 -- 6.
+List the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach
+
+SELECT mdate, teamname
+FROM game JOIN eteam ON (team1=eteam.id)
+WHERE coach = 'Fernando Santos';
 
 -- 7.
 
