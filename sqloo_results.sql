71f28694a497f1bfb6c008f2158c4db18936f2a4
Your Name <you@example.com>
2020-11-16 08:51:26 -0500

    finished sql zoo from laptop

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -41,11 +41,16 @@ SELECT name FROM world
  WHERE name LIKE '____'
 
 -- 10.
-
+SELECT name FROM world
+ WHERE name LIKE '____'
 -- 11.
-
+SELECT name
+  FROM world
+ WHERE name LIKE capital
 -- 12.
-
+SELECT name
+FROM world
+WHERE capital LIKE '%city'
 -- 13, 14, and 15 have a bug, please skip them
 
 
@@ -64,67 +69,143 @@ SELECT name, population/1000000
 FROM world
 WHERE continent LIKE 'South America'
 -- 5.
-
+SELECT name, population
+FROM world
+WHERE name IN ('France','Germany','Italy')
 -- 6.
-
+SELECT name
+FROM world
+WHERE name LIKE '%United%'
 -- 7.
-
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 or population > 250000000
 -- 8.
-
+SELECT name, population, area
+FROM world
+WHERE (area > 3000000 or population > 250000000)
+and not(area > 3000000 and population > 250000000)
 -- 9.
+SELECT name, ROUND((population/1000000), 2), ROUND((gdp/1000000000), 2)
+FROM world
+WHERE continent LIKE 'South America'
 
 -- 10.
-
+SELECT name, ROUND((gdp/population)/ 1000) * 1000
+FROM world
+WHERE (gdp >= 1000000000000)
 -- 11.
+SELECT name, capital
+FROM world
+WHERE LENGTH(name) = LENGTH(capital)
 
 -- 12.
-
+SELECT name, capital
+FROM world
+WHERE LEFT(name,1) = LEFT(capital,1) and not (name = capital)
 -- 13.
-
-
-
+SELECT name
+FROM world
+WHERE name LIKE '%a%'
+AND name LIKE '%e%'
+AND name LIKE '%i%'
+AND name LIKE '%o%'
+AND name LIKE '%u%'
+AND name NOT LIKE '% %'
 -- 3 SELECT from Nobel
 -- 1.
-
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
 -- 2.
-
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
 -- 3.
-
+SELECT yr, subject
+FROM nobel
+WHERE winner LIKE 'Albert Einstein'
 -- 4.
-
+SELECT winner
+FROM nobel
+WHERE subject LIKE 'Peace'
+AND yr >= 2000
 -- 5.
-
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject LIKE 'Literature'
+AND yr <= 1989
+AND yr >= 1980
 -- 6.
-
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter',
+                   'Barack Obama')
 -- 7.
-
+SELECT winner
+FROM nobel
+WHERE winner LIKE 'John%'
 -- 8.
-
+SELECT yr, subject, winner
+FROM nobel
+WHERE (subject LIKE 'Physics' and yr = 1980) or (subject LIKE 'Chemistry' and yr = 1984)
 -- 9.
-
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1980
+AND (subject NOT LIKE 'Chemistry') and (subject NOT LIKE 'Medicine')
 -- 10.
-
+SELECT *
+FROM nobel
+WHERE (subject LIKE 'Medicine' and yr < 1910) or (subject LIKE 'Literature' and yr >= 2004)
 -- 11.
-
+SELECT *
+FROM nobel
+WHERE winner LIKE 'PETER GRÜNBERG'
 -- 12.
-
+SELECT *
+FROM nobel
+WHERE winner LIKE ('EUGENE O%')
 -- 13.
-
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC
 -- 14.
-
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject IN ('Physics','Chemistry'), subject, winner
 
 
 -- 4 SELECT within SELECT
 -- 1.
-
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
 -- 2.
-
+SELECT name from world
+WHERE continent='Europe' AND gdp/population >
+(SELECT gdp/population FROM world
+WHERE gdp/population 
+AND name = 'United Kingdom')
 -- 3.
-
+SELECT name, continent
+FROM world
+WHERE continent IN (SELECT continent FROM world WHERE name in ('Argentina', 'Australia'))
+ORDER BY name
 -- 4.
-
+SELECT name, population FROM world
+WHERE population > 
+(SELECT population FROM world WHERE name = 'CANADA')
+AND population < (SELECT population from world WHERE name = 'Poland')
 -- 5.
-
+SELECT name, CONCAT(ROUND(100*population/(SELECT population FROM world WHERE name = 'Germany'),0) , '%')
+FROM world
+WHERE continent = 'Europe'
 -- As noted in the tutorial, questions 6 - 10 in this unit are considered bonuses, just like units 6 - 8+
 -- 6.
 
@@ -140,22 +221,36 @@ WHERE continent LIKE 'South America'
 
 -- 5 SUM and COUNT
 -- 1.
-
+SELECT SUM(population)
+FROM world
 -- 2.
-
+SELECT DISTINCT(continent)
+FROM world
 -- 3.
-
+SELECT SUM(gdp)
+FROM world
+WHERE continent = 'Africa'
 -- 4.
-
+SELECT COUNT(name)
+FROM world
+WHERE area > 1000000
 -- 5.
-
+SELECT SUM(population)
+FROM world
+WHERE name = 'Estonia' OR name = 'Latvia' or name = 'Lithuania'
 -- 6.
-
+SELECT continent, COUNT(name)
+FROM world
+GROUP BY continent
 -- 7.
-
+SELECT continent, COUNT(name)
+FROM world
+WHERE population > 10000000 GROUP BY continent
 -- 8.
-
-
+SELECT DISTINCT(continent)
+FROM world
+GROUP BY continent
+HAVING SUM (population) > 100000000
 -- Note: the units below this are bonus for this weekend, and they will be required in a future assignment. If you do them now you will be ahead of the game!
 -- 6 JOIN
 -- 1.
@@ -257,4 +352,3 @@ WHERE continent LIKE 'South America'
 -- 7.
 
 -- 8.
-
