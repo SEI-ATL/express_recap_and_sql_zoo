80b810b106eeae339edfea41964956ca9cb7acd0
jtreeves <jackson.t.reeves@gmail.com>
2020-11-16 05:06:57 -0500

    [sql] Finish majority of question 5

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -216,19 +216,31 @@ WHERE y.continent = x.continent)
 
 -- 5 SUM and COUNT
 -- 1.
-
+SELECT SUM(population)
+FROM world
 -- 2.
-
+SELECT DISTINCT continent FROM world
 -- 3.
-
+SELECT SUM(gdp)
+FROM world
+WHERE continent = 'Africa'
 -- 4.
-
+SELECT COUNT(name)
+FROM world
+WHERE area >= 1000000
 -- 5.
-
+SELECT SUM(population)
+FROM world
+WHERE name IN ('Estonia', 'Latvia', 'Lithuania')
 -- 6.
-
+SELECT continent, COUNT(name)
+FROM world
+GROUP BY continent
 -- 7.
-
+SELECT continent, COUNT(name)
+FROM world
+WHERE population >= 10000000
+GROUP BY continent
 -- 8.
 
 
