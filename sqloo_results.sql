4af537c867029fe7ff28725e25fc340dfe67f6d4
delayedaa <delayedaa@gmail.com>
2020-11-15 22:39:47 -0600

    [sqloo_results.sql] Add answers to unit 8

--- sqloo_results.sql
+++ sqloo_results.sql
@@ -734,10 +734,65 @@ FROM teacher;
 
 -- 8+ Numeric Examples
 -- 1.
+SELECT A_STRONGLY_AGREE
+FROM nss
+WHERE question = 'Q01'
+  AND institution = 'Edinburgh Napier University'
+  AND subject = '(8) Computer Science';
+
 -- 2.
+SELECT institution, subject
+FROM nss
+WHERE question = 'Q15'
+  AND score >= 100;
+
 -- 3.
+SELECT institution, score
+FROM nss
+WHERE subject = '(8) Computer Science'
+  AND question = 'Q15'
+  AND score < 50;
+
 -- 4.
+SELECT subject, SUM(response)
+FROM nss
+WHERE question = 'Q22'
+  AND (subject = '(8) Computer Science'
+  OR subject = '(H) Creative Arts and Design')
+GROUP BY subject;
+
 -- 5.
+SELECT subject, SUM((response * A_STRONGLY_AGREE) / 100)
+FROM nss
+WHERE question = 'Q22'
+  AND (subject = '(8) Computer Science'
+  OR subject = '(H) Creative Arts and Design')
+GROUP BY subject;
+
 -- 6.
+SELECT subject, ROUND(SUM((response * A_STRONGLY_AGREE) / 100) / SUM(response) * 100, 0)
+FROM nss
+WHERE question = 'Q22'
+  AND (subject = '(8) Computer Science'
+  OR subject = '(H) Creative Arts and Design')
+GROUP BY subject;
+
 -- 7.
+SELECT institution, ROUND((SUM((score * response) / 100) / SUM(response)) * 100, 0)
+FROM nss
+WHERE question = 'Q22'
+  AND institution LIKE '%Manchester%'
+GROUP BY institution;
+
 -- 8.
+SELECT institution, SUM(sample), (
+  SELECT SUM(sample)
+  FROM nss y
+  WHERE subject = '(8) Computer Science'
+  AND x.institution = y.institution
+  AND question = 'Q01'
+)
+FROM nss x
+WHERE institution LIKE '%Manchester%'
+  AND question = 'Q01'
+GROUP BY institution;
