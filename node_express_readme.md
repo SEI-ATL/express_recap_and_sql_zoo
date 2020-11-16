e7880503a6ec6405ee68adc7b5507ad673f7dba1
Richard Leung <michelleli@MMac.local>
2020-11-16 02:52:21 -0500

    Update

--- node_express_readme.md
+++ node_express_readme.md
@@ -1,1 +1,39 @@
 <!-- When you're finished updating your node & express readme, copy-paste it into here for submission! -->
+
+## Express
+[Instructions](https://romebell.gitbook.io/sei-1019/node-express/00readme-1/01intro-to-express)
+1. Create a repo and `clone` in local server
+2. Type `npm i express` in terminal to install express
+3. Create a index.js
+4. Add code to `index.js` to import the express module and create an express application
+```js
+const express = require('express');
+const app = express();
+```
+5. Add code to `index.js` to create a home route. 
+- .get() is an express method that takes in two parameters:
+    1. a URL Pattern
+    2. a callback function that tells the app what to do when this route is reached
+- res.send() sends back a simple string
+```js
+app.get('/', function(req, res) {
+    res.send('Hello, World!');
+});
+```
+6. Add `app.listen(8000);` to `index.js` to tell the application to listen to port 8000
+7. Type `nodemon index.js` or `node index.js` in terminal to run
+8. Type `localhost:8000` in browser to visit your application
+
+## EJS: Embedded Javascript
+[Instructions](https://romebell.gitbook.io/sei-1019/node-express/00readme-1/01intro-to-express/04templates)
+1. Type `npm install ejs` in terminal to add ejs to project
+2. Add `app.set('view engine', 'ejs');` to `index.js` before the routes and after creating application.
+3. Rename the .html files to .ejs files and place in a `views` folder
+4. Change route to:
+```js
+app.get('/(filename)', function(req, res) {
+  res.render('(filename)', object);
+});
+```
+5. The render function can take in an object as a parameter and access the values stored in it as variables inside the ejs template
+6. Use `<%= (embedded js goes here) %>` to embed any javascript in ejs files
--- sqloo_results.sql
+++ sqloo_results.sql
@@ -1,110 +1,170 @@
 -- 0 SELECT basics
 -- 1.
-
+SELECT population FROM world
+  WHERE name = 'Germany';
 -- 2.
-
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
 -- 3.
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000;
 
 -- 1 SELECT name
-
 -- 1.
-
+SELECT name FROM world
+  WHERE name LIKE 'y%';
 -- 2.
-
+SELECT name FROM world
+  WHERE name LIKE '%y';
 -- 3.
-
+SELECT name FROM world
+  WHERE name LIKE '%x%';
 -- 4.
-
+SELECT name FROM world
+  WHERE name LIKE '%land';
 -- 5.
-
+SELECT name FROM world
+  WHERE name LIKE 'c%ia';
 -- 6.
-
+SELECT name FROM world
+  WHERE name LIKE '%oo%';
 -- 7.
-
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%';
 -- 8.
-
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name;
 -- 9.
-
+SELECT name FROM world
+ WHERE name LIKE '%o__o%';
 -- 10.
-
+SELECT name FROM world
+ WHERE name LIKE '____';
 -- 11.
+SELECT name FROM world
+ WHERE name = capital;
 
 -- 12.
-
+SELECT name FROM world
+ WHERE capital = concat(name, ' city');
 -- 13, 14, and 15 have a bug, please skip them
 
 
 -- 2 SELECT from World
 -- 1.
-
+SELECT name, continent, population FROM world;
 -- 2.
-
+SELECT name FROM world
+ WHERE population >= 200000000;
 -- 3.
-
+SELECT name, gdp/population FROM world
+ WHERE population >= 200000000;
 -- 4.
-
+SELECT name, population/1000000 FROM world
+ WHERE continent = 'South America';
 -- 5.
-
+SELECT name, population FROM world
+ WHERE name IN ('France', 'Germany', 'Italy');
 -- 6.
-
+SELECT name FROM world
+ WHERE name LIKE 'United %';
 -- 7.
-
+SELECT name, population, area FROM world
+ WHERE area > 3000000 OR population > 250000000;
 -- 8.
-
+SELECT name, population, area FROM world
+ WHERE area > 3000000 XOR population > 250000000;
 -- 9.
-
+SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) FROM world
+ WHERE continent = 'South America';
 -- 10.
-
+SELECT name, ROUND(GDP/population, -3) FROM world
+ WHERE gdp >= 1000000000000;
 -- 11.
-
+SELECT name, capital FROM world
+ WHERE LENGTH(name) = LENGTH(capital);
 -- 12.
-
+SELECT name, capital FROM world
+WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital;
 -- 13.
-
-
+SELECT name FROM world
+ WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name NOT LIKE '% %';
 
 -- 3 SELECT from Nobel
 -- 1.
-
+SELECT yr, subject, winner FROM nobel
+ WHERE yr = 1950;
 -- 2.
-
+SELECT winner FROM nobel
+ WHERE yr = 1962 AND subject = 'Literature'
 -- 3.
-
+SELECT yr, subject FROM nobel
+ WHERE winner = 'Albert Einstein';
 -- 4.
-
+SELECT winner FROM nobel
+ WHERE yr >= 2000 AND subject = 'Peace';
 -- 5.
-
+SELECT yr, subject, winner FROM nobel
+ WHERE yr <= 1989 AND yr >= 1980 AND subject = 'Literature';
 -- 6.
-
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama');
 -- 7.
-
+SELECT winner FROM nobel
+  WHERE winner LIKE 'John %';
 -- 8.
-
+SELECT yr, subject, winner FROM nobel
+WHERE (subject = 'physics' AND yr = 1980) OR (subject = 'chemistry' AND yr = 1984);
 -- 9.
-
+SELECT yr, subject, winner FROM nobel
+WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine');
 -- 10.
-
+SELECT yr, subject, winner FROM nobel
+ WHERE (subject = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004);
 -- 11.
-
+SELECT * FROM nobel
+ WHERE winner = 'PETER GRÜNBERG';
 -- 12.
-
+SELECT * FROM nobel
+ WHERE winner = 'EUGENE O''NEILL';
 -- 13.
-
+SELECT winner, yr, subject FROM nobel
+ WHERE winner LIKE 'Sir %'
+ORDER BY yr DESC, winner;
 -- 14.
-
-
+SELECT winner, subject FROM nobel
+ WHERE yr = 1984
+ ORDER BY subject IN ('Physics', 'Chemistry'), subject, winner;
 
 -- 4 SELECT within SELECT
 -- 1.
-
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name = 'Russia');
 -- 2.
-
+SELECT name FROM world
+  WHERE continent = 'Europe' AND GDP/population >
+     (SELECT GDP/population FROM world
+      WHERE name = 'United Kingdom');
 -- 3.
-
+SELECT name, continent FROM world
+ WHERE continent IN 
+  (SELECT continent FROM world
+   WHERE name IN ('Australia', 'Argentina'))
+ORDER BY name;
 -- 4.
-
+SELECT name FROM world
+ WHERE population > 
+  (SELECT population FROM world WHERE name = 'Canada')
+ AND population <
+  (SELECT population FROM world WHERE name = 'Poland');
 -- 5.
-
+SELECT name, CONCAT(ROUND(100 * population/(SELECT population FROM world 
+ WHERE name = 'Germany')), '%')
+FROM world
+ WHERE continent = 'Europe';
 -- As noted in the tutorial, questions 6 - 10 in this unit are considered bonuses, just like units 6 - 8+
 -- 6.
 
@@ -120,21 +180,29 @@
 
 -- 5 SUM and COUNT
 -- 1.
-
+SELECT SUM(population) FROM world;
 -- 2.
-
+SELECT DISTINCT(continent) FROM world;
 -- 3.
-
+SELECT SUM(gdp) FROM world
+ WHERE continent = 'Africa';
 -- 4.
-
+SELECT COUNT(name) FROM world
+ WHERE area >= 1000000;
 -- 5.
-
+SELECT SUM(population) FROM world
+ WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
 -- 6.
-
+SELECT continent, COUNT(name) FROM world
+ GROUP BY continent;
 -- 7.
-
+SELECT continent, COUNT(name) FROM world
+ WHERE population >= 10000000
+ GROUP BY continent;
 -- 8.
-
+SELECT continent FROM world
+ GROUP BY continent
+ HAVING SUM(population) >= 100000000;
 
 -- Note: the units below this are bonus for this weekend, and they will be required in a future assignment. If you do them now you will be ahead of the game!
 -- 6 JOIN
